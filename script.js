a1c00e78019aaedcbd827da415edd7f6e4732a90
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-09-15 20:52:59 -0400

    Touched up JavaScript

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let symbolAscii3 = populateArray(91,96);
 let lowercaseAscii = populateArray(97, 122);
 let uppercaseAscii = populateArray(65, 90); 
 let numberAscii = populateArray(48, 57);
-
 let symbolAsciiTotal = symbolAscii1.concat(symbolAscii2,symbolAscii3);
 
 let passwordGenArray = [];
@@ -25,71 +24,71 @@ let passwordGenArray = [];
 let generatePassword = function () {
     let passwordLength = prompt("How many characters does the password need to be?");
 
-    if (passwordLength < "8" && passwordLength > "128") {
-        alert("Enter a valid number")
+    let uppercaseSelect = "No";
+    let lowercaseSelect = "No";
+    let numberSelect = "No";
+    let symbolSelect = "No";
+
+    let password = [];
+
+    passwordLength = parseInt(passwordLength);
+
+    if (!passwordLength) {
+        alert("Enter a +alid number")
+        return 0;
+    }
+
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("Enter a !alid number")
         return 0;
     } 
 
-    if (!passwordLength) {
-        alert("Enter a valid number")
-        return 0;
-    }
-
     if (isNaN(passwordLength)) {
-        alert("Enter a Valid number")
+        alert("Enter a Walid number")
         return 0;
     }
 
-    let uppercaseSelect = prompt("Do you want uppercase characters? Y/N");
-
-    let lowercaseSelect = prompt("Do you want lowercase characters? Y/N");
-
-    let numericSelect = prompt("Do you want numbers in your password? Y/N");
-
-    let symbolSelect = prompt("Do you want any special symbols in your password? Y/N");
-
-    uppercaseSelect = uppercaseSelect.toUpperCase();
-
-    lowercaseSelect = lowercaseSelect.toUpperCase();
-
-    numericSelect = numericSelect.toUpperCase();
-
-    symbolSelect = symbolSelect.toUpperCase();
-
-    if (!confirm(" You have chosen Uppercase: "+uppercaseSelect+" Lowercase: "+lowercaseSelect+" Numbers: "+numericSelect+" Symbols: "+symbolSelect+". Press okay to continue or cancel to start again")) {
-        return 0;
-    }
-
-    if (uppercaseSelect === "Y") {
+    if (confirm("Do you want uppercase letters?")) {
+        uppercaseSelect = "Yes";
         for (let i = 0; i < uppercaseAscii.length; i++) {
             passwordGenArray.push(uppercaseAscii[i]);
         }
     }
 
-    if (lowercaseSelect === "Y") {
+    if (confirm("Do you want lowercase letters?")) {
+        lowercaseSelect = "Yes";
         for (let i = 0; i < lowercaseAscii.length; i++) {
             passwordGenArray.push(lowercaseAscii[i]);
         }
     }
 
-    if (numericSelect === "Y") {
+    if (confirm("Do you want numbers in your password?")) {
+        numberSelect = "Yes";
         for (let i = 0; i < numberAscii.length; i++) {
             passwordGenArray.push(numberAscii[i]);
         }
     }
-    if (symbolSelect === "Y") {
+    if (confirm("Do you want symbols in your password?")) {
+        symbolSelect = "Yes";
         for (let i = 0; i < symbolAsciiTotal.length; i++) {
             passwordGenArray.push(symbolAsciiTotal[i]);
         }
     }
-    let password = [];
+
+    if (uppercaseSelect === "No" && lowercaseSelect === "No" && numberSelect === "No" && symbolSelect === "No") {
+        alert("You must select at least one of the conditionals");
+        return 0;
+    }
+
+    if (!confirm(" You have chosen Uppercase: "+uppercaseSelect+". Lowercase: "+lowercaseSelect+". Numbers: "+numberSelect+". Symbols: "+symbolSelect+". Press okay to continue or cancel to start again")) {
+        return 0;
+    }
 
     for (let i = 0; i < passwordLength; i++) {
         let character = passwordGenArray[Math.floor(Math.random() * passwordGenArray.length)];
         password.push(String.fromCharCode(character));
     }
 
-    console.log(password);
     return password.join('');
 
 }
